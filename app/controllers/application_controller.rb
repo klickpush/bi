1d29efb6e604301744bff9509f8cdd69ac6f88d6
James Thullbery <ifndef@gmail.com>
2014-08-09 17:20:10 -0400

    logging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,18 @@ class ApplicationController < ActionController::Base
   before_filter :ensure_logged_in
 
   def ensure_logged_in
+    console.log('Ensured Logged In Begin')
     unless (request.referer.blank? && session['redirected'].present?)
+      console.log('Request: ' + request.referer)
       if request.referer.present? && (URI.parse(request.referer).host.include?('0.0.0.0')  || URI.parse(request.referer).host.include?('klickpush.com') )
+        console.log('Setting Session')
         session['redirected'] = 1
       else
+        console.log('Failed to set session')
         reset_session
         redirect_to 'https://www.klickpush.com'
       end
     end
+    console.log('Ensured Logged In End')
   end
 end
